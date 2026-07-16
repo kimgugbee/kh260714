f0050b2922ae84e3be20bfd5e3d91c451e3b18d4
DESKTOP-6FOOJIU\user1 <zzz@yaho.zzz>
2026-07-16 16:29:32 +0900

    python

---
+++ python/prj02/test03.py
@@ -0,0 +1,8 @@
+x = {"name":"홍길동" , "age":20 , "blood":"A"}
+# x["name"] = "홍길동"
+# x["age"] = 20
+x["age"] = 30
+print(x)
+del x["age"]
+print(x)
+print("age" in x)
---
+++ python/prj02/test04.py
@@ -0,0 +1,11 @@
+# set
+x = {10,20,30,20,30}
+print(x)
+print(type(x))
+
+x.add(123)
+x.add(777)
+print(x)
+
+x.remove(777)
+print(x)
